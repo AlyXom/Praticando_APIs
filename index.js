9eea4dc2d42051623ff2d91c374a82cabe5410bb
Alysson Araujo <alyssoncraft12@gmail.com>
2022-11-23 16:02:27 -0300

    Update API

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
+app.use(express.json())
+
+let users = [
+    {
+        id: 1,
+        name: "Alysson",
+        age: 19,
+        city: "São Paulo"
+    }
+]
 
 app.listen(3000, console.log('Rodando servidor'))
 
-app.get('/Usuario', (req,res)=>{
-    return res.json({Usuario: "Alysson"})
+app.get('/api', (req,res)=>{
+    return res.json(users)
+})
+
+app.route('/api').post((req,res) =>{
+    const lastId = users[users.length - 1].id;
+    const newUser = {
+        id: lastId + 1,
+        nome: req.body.name,
+        idade: req.body.age,
+        cidade: req.body.city,
+    }
 })
